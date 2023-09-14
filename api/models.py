120d2f1532f857b8b0f01fe61226ef2069bc5c64
cerdowizard <holuwayinkzzy@gmail.com>
2023-09-14 12:06:26 +0100

    Authentication

--- api/models.py
+++ api/models.py
@@ -61,15 +61,17 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+
 class Cart(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    
+
     def __str__(self):
         return f"Cart for {self.user.username}"
-    
+
+
 class CartItem(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -129,9 +131,11 @@ class Shipping(models.Model):
     estimated_delivery_date = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return f"Shipping for Order #{self.order.id}"
-    
+
+
 class Payment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     order = models.OneToOneField('Order', on_delete=models.CASCADE)
@@ -140,5 +144,13 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Completed', 'Completed')])
     payment_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class SecretTokenView(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- api/views.py
+++ api/views.py
@@ -1,15 +1,10 @@
-from django.http import Http404
-from django.shortcuts import render
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, status
+from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import permissions
-from rest_framework.pagination import PageNumberPagination
-from api.models import User
-from rest_framework import generics, status
-from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+
 from api.serializers import *
 
 
